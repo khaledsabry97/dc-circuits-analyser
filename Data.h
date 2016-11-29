e1e6c0b43f603311be8521a29ce35e2c396ed9ae
Mahmoud Adas <mido3ds@gmail.com>
2016-11-29 12:55:35 +0200

    init

--- Data.h
+++ Data.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <list>
 using namespace std;
 
 /* PROTOTYPES */
@@ -8,8 +9,6 @@ class Element
 private:
     enum Type {R, E, J};
 
-    Element* _next = nullptr;
-    Element* _prev = nullptr;
     Type _type = R;
     int _id = -1;
     double _value = 0;
@@ -20,12 +19,10 @@ private:
 
 public:
     Element(const char &t, const int &id, const double &v);
+	~Element();
+
     char GetType();
     void ChangeType(const char &c);
-    Element* GetNext();
-    Element* GetPrev();
-    void SetNext(Element* n);
-    void SetPrev(Element* n);
     int GetId();
     void ChangeId(const int &num);
     double GetValue();
@@ -35,27 +32,21 @@ public:
 class Node 
 {
 private:
-    Node* _next = nullptr;
-    Element* _firstElement = nullptr;
     const int _id;
     double _volt = 0;
     int _numElements = 0;
 
-    void _SetFirstElem(Element* n);
-
 public:
+	Node(const int &id);
+	~Node();
+	
     int GetId();
     void ChangeVolt(const double &v);
     double GetVolt();
     int GetNumOfElem();
     bool IsEssential();
-    Element* GetFirstElem();
-    Node* GetNext();
-    void SetNext(Node* n);
     void Add(Element* e);
     void Remove(Element* e);
-    Node(const int &id);
-	~Node();
 };
 
 class Circuit 
@@ -63,16 +54,15 @@ class Circuit
 private:
     Node* _firstNode = nullptr;
     Node* _lastNode = nullptr;
-	int _numNodes = 0;
 
 public:
+	Circuit();
+	~Circuit();
+
     bool Add(Node* n);
     bool Remove(Node* n);
-    Node* GetLastNode();
-    Node* GetFirstNode();
 	int GetNumOfNodes();
-    //void Read();
-	~Circuit();
+    void Read();
 	void Push_back(Node* n);
 	void Push_front(Node* n);
 	bool Pop_back();
