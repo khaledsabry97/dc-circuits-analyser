1b459aace3c6b4189ea8aecfb7045efad6c0e5bc
Mahmoud Adas <mido3ds@gmail.com>
2017-01-22 23:57:22 +0200

    works on windows, but without colors

--- Headers/Colors.h
+++ Headers/Colors.h
@@ -1,9 +1,16 @@
 /*  colors used by program  */
 
-#define RED "\033[0;31m"
-#define BLUE "\033[0;34m"
-#define GREEN "\033[0;32m"
-#define YELLOW "\033[1;33m"
-#define CYAN "\033[0;36m"
+//#define RED "\033[0;31m"
+//#define BLUE "\033[0;34m"
+//#define GREEN "\033[0;32m"
+//#define YELLOW "\033[1;33m"
+//#define CYAN "\033[0;36m"
 
-#define WHITE "\033[0m"
+#define RED ""
+#define BLUE ""
+#define GREEN ""
+#define YELLOW ""
+#define CYAN ""
+
+//#define WHITE "\033[0m"
+#define WHITE ""
