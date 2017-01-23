eed9a2c9d01ac505fe2798878640d88b51eb6d38
Mahmoud Adas <mido3ds@gmail.com>
2017-01-23 19:50:09 +0200

    added support for colors for non-windows

--- Headers/Colors.h
+++ Headers/Colors.h
@@ -1,16 +1,19 @@
 /*  colors used by program  */
 
-//#define RED "\033[0;31m"
-//#define BLUE "\033[0;34m"
-//#define GREEN "\033[0;32m"
-//#define YELLOW "\033[1;33m"
-//#define CYAN "\033[0;36m"
+#if defined(_WIN32)
+    #define RED ""
+    #define BLUE ""
+    #define GREEN ""
+    #define YELLOW ""
+    #define CYAN ""
 
-#define RED ""
-#define BLUE ""
-#define GREEN ""
-#define YELLOW ""
-#define CYAN ""
+    #define WHITE ""
+#else
+    #define RED "\033[0;31m"
+    #define BLUE "\033[0;34m"
+    #define GREEN "\033[0;32m"
+    #define YELLOW "\033[1;33m"
+    #define CYAN "\033[0;36m"
 
-//#define WHITE "\033[0m"
-#define WHITE ""
+    #define WHITE "\033[0m"
+#endif
